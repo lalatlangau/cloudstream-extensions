6ac86a949ea8988d6f92d7f3b08928735774c7b8
lalatlangau <163626123+lalatlangau@users.noreply.github.com>
2026-06-24 15:00:15 +0800

    chore: update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,6 @@
-// Top-level build file — jangan edit secara langsung
 buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") }
-    }
+    repositories { google(); mavenCentral(); maven { url = uri("https://jitpack.io") } }
 }
-
 allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") }
-    }
+    repositories { google(); mavenCentral(); maven { url = uri("https://jitpack.io") } }
 }
