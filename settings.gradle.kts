ab3a59461fda7062b7e89bf19d85cff710a6bb94
lalatlangau <163626123+lalatlangau@users.noreply.github.com>
2026-06-24 15:00:35 +0800

    chore: update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,4 @@
-// settings.gradle.kts
-// Nama dalam include() MESTI sama persis dengan nama folder dalam repo
 rootProject.name = "cloudstream-extensions"
 
-include(":Anichin")
 include(":Pencurimoviesubmalay")
+include(":Anichin")
