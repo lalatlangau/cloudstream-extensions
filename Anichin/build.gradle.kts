ed23e376209eb4aa241fb93d671aa2561a00a766
lalatlangau <163626123+lalatlangau@users.noreply.github.com>
2026-06-24 15:00:06 +0800

    chore: update Anichin/build.gradle.kts

--- Anichin/build.gradle.kts
+++ Anichin/build.gradle.kts
@@ -3,8 +3,7 @@ version = 1
 cloudstream {
     language    = "id"
     description = "Provider untuk Anichin – Fansub Donghua Subtitle Indonesia"
-    authors     = listOf("Microtech.Store (MTS)")
-    status      = 1 // 0=down, 1=ok, 2=slow, 3=beta
-
-    iconUrl = "${mainUrl}/favicon.ico"
+    authors     = listOf("MTS")
+    status      = 1
+    iconUrl     = "${mainUrl}/favicon.ico"
 }
